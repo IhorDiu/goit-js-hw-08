756a69b220eaf86eb124e764fb31c476cd897828
IhorDiu <110743288+IhorDiu@users.noreply.github.com>
2022-12-27 01:25:24 +0200

    Task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,53 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+console.log("SimpleLightbox", SimpleLightbox)
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            title="${description}"
+            alt="${description}"
+            >
+        </a>
+    `,
+  '',
+);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+new SimpleLightbox('.gallery a', { captionDelay: 250});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ТУТАРІАЛ ПО ДОКІ
+// 1 Install - npm install simplelightbox
+// 2 Як зробити import - import SimpleLightbox from "simplelightbox";
+// 3 Перевірка console.log(SimpleLightbox)
+// 4 Як використати Usage - new SimpleLightbox('.some-element a', { /* options */ });
+// 5 Розбір опцій Options
+// 6 Методи роботи з інстансом(екземпяром)
+
